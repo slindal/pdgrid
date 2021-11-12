6a1dadcc77201ed209cb7f240c7db07eae1936eb
Svein Lindal <svein.lindal@gmail.com>
2021-11-12 12:17:13 -0500

    added redis caching.

--- test/unit/test_unique_values.py
+++ test/unit/test_unique_values.py
@@ -4,10 +4,5 @@ from pdgrid.pdgrid import unique_values, expand_nodes
 def test_unique_values():
     df = pd.DataFrame(columns=["a", "b"], data=[[1,2], [1,2], [1,3], [1,4]])
     
-    uv = unique_values(df, "a")
-    assert(unique_values(df, "a") == {"a": [1]})
-    assert(unique_values(df, "b") == {"b": [2,3,4]})
-
-    assert(unique_values(df, ["a", "b"]) == { "a": [1],
-                                              "b": [2,3,4] })
-    
+    assert(unique_values(df, "a") == ["1"])
+    assert(unique_values(df, "b") == ["2","3","4"])
