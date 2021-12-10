765f025b7839514877fbf1ed54bf9757da0401a5
Svein Lindal <svein.lindal@gmail.com>
2021-12-10 12:45:03 -0500

    Process expanded nodes with filters. Handle numerical filters

--- test/functional/test_filter.py
+++ test/functional/test_filter.py
@@ -29,17 +29,15 @@ def test_1():
     response = aggrid_values(df, group_model)
     rows = response['rows']
     assert(len(rows)) == 5
-    assert(rows ==  [{'country': 'Australia', 'gold': '0'}, {'country': 'China', 'gold': '3'}, {'country': 'Romania', 'gold': '5'}, {'country': 'South Korea', 'gold': '1'}, {'country': 'United States', 'gold': '4'}]
+    assert(rows ==  [{'country': 'Australia', 'gold': 0}, {'country': 'China', 'gold': 3}, {'country': 'Romania', 'gold': 5}, {'country': 'South Korea', 'gold': 1}, {'country': 'United States', 'gold': 4}]
 )
-
 
 
 def test_filterendpoint():
     df = pandas.read_json("/home/slindal/dev/pdgrid/test/manual/olympic-winners.json")
     response = unique_values(df, 'age')
-    assert(response == ['15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '37', '41'])
+    assert(response == [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 37, 41])
     
-
 
 def test_text_filter_contains():
     df = pandas.DataFrame(columns=['a', 'b', 'c'], data = [['aaa', 'bbb', 'ccc'],
@@ -195,4 +193,59 @@ def test_multi_filter():
 
 
 
+def test_number_filter():
+
+    df = pandas.DataFrame(columns=['a', 'b', 'c'], data = [[1, 'bbb', 'ccc'],
+                                                             [21, 'bba', 'cca'],
+                                                             [22, 'baa', 'cbb']])
+
+    request = {
+        "startRow": 0,
+        "endRow": 100,
+        "rowGroupCols": [],
+        "valueCols": [],
+        "pivotCols": [],
+        "pivotMode": False,
+        "groupKeys": [],
+        "filterModel": {
+            "b": {
+                "filterType":"text",
+                "operator":"OR",
+                "condition1": {"filterType": "text", "type": "endsWith", "filter": "ba"},
+                "condition2": {"filterType": "text", "type": "endsWith", "filter": "aa"},
+            },
+            "a": {"filterType": "number", "type": "lessThanOrEqual", "filter": 21}
+
+        },                                  
+        "sortModel": []
+    }
+
+    response = aggrid_values(df, request)
+    assert(response == {"rows": [{"a": 21, "b": "bba", "c": "cca"}], "lastRow": 1})
+
     
+
+def test_number_inRange_filter():
+
+    df = pandas.DataFrame(columns=['a', 'b', 'c'], data = [[1, 'bbb', 'ccc'],
+                                                           [2, 'bba', 'cca'],
+                                                           [3, 'baa', 'cbb'],
+                                                           [4, 'bb', 'bb']
+                                                           ])
+
+    request = {
+        "startRow": 0,
+        "endRow": 100,
+        "rowGroupCols": [],
+        "valueCols": [],
+        "pivotCols": [],
+        "pivotMode": False,
+        "groupKeys": [],
+        "filterModel": {
+            "a": {"filterType": "number", "type": "inRange", "filter": 1, "filterTo": 3}
+        },                                  
+        "sortModel": []
+    }
+
+    response = aggrid_values(df, request)
+    assert(response == {"rows": [{"a": 1, "b": "bbb", "c": "ccc"}, {"a": 2, "b": "bba", "c": "cca"}], "lastRow": 2})
