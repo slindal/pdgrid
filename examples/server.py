8a17766ee78aa60ebfecc50af8b69c9aa0e80e95
Svein Lindal <svein.lindal@gmail.com>
2022-01-04 11:58:53 -0500

    make redis caching optional

--- examples/server.py
+++ examples/server.py
@@ -6,42 +6,43 @@ import io
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_caching import Cache
-from flask import current_app
+from flaskext.mysql import MySQL
 
 from pdgrid import unique_values, aggrid_values
 
-
-from flask import Flask
 app = Flask(__name__)
-from flaskext.mysql import MySQL
 
 CORS(app)
 
 mysql = MySQL()
+app.config['MYSQL_DATABASE_PASSWORD'] = 'PASSWORD' ## Update to your password
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'PASSWORD'
 app.config['MYSQL_DATABASE_DB'] = 'sample_data'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
 
-app.config['CACHE_TYPE'] = 'RedisCache'
+cache = None
+#Uncomment these lines to use redis caching. Requires installation of redis server.
+#app.config['CACHE_TYPE'] = 'RedisCache'
+#cache = Cache(app)
 
-cache = Cache(app)
-
-def load_from_db(dataset):
+def load_from_db():
     conn = mysql.connect()
     df = pd.read_sql_query("SELECT athlete, age, country, year, date, sport, gold, silver, bronze, total from athletes", conn)
     return df
         
 
 def load_dataframe(dataset):
+    if not cache:
+        return load_from_db()
+
     buffer = cache.get(dataset)
     if buffer:
         df = pd.read_parquet(io.BytesIO(buffer))
         return df
     else:
-        df = load_from_db(dataset)
+        df = load_from_db()
         buffer = io.BytesIO()
         df.to_parquet(buffer)
         buffer.seek(0)
@@ -62,4 +63,3 @@ def filter_field(dataset, filter_field):
     df = load_dataframe(dataset)
     data = unique_values(df, filter_field)
     return jsonify(data)
-
