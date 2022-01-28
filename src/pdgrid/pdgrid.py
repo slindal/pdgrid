b3deff8cd427a95e1e1461fab74d10a5328e0aa2
Svein Lindal <svein.lindal@gmail.com>
2022-01-28 12:10:04 -0500

    Change python version, minor refactor

--- src/pdgrid/pdgrid.py
+++ src/pdgrid/pdgrid.py
@@ -36,8 +36,7 @@ def pivot(df, request, response):
     else:
         df.columns = df.columns.swaplevel(0,1)
     
-    secondaryColDefs = [
-]
+    secondaryColDefs = []
     for group, cols in itertools.groupby(sorted(df.columns, key=lambda k: k[0]), key=lambda k: k[0]):
         secondaryColDefs.append({'groupId': str(group),
                                  'headerName': str(group),
@@ -49,14 +48,41 @@ def pivot(df, request, response):
                                  }
                                 )
     df.columns = ["|".join(str(f) for f in col) for col in df.columns] 
-    
     df = df.replace({np.nan: None})
 
     response['secondaryColDefs'] = secondaryColDefs
     return df
-    
-def sort_and_aggregate(df, request, response):
 
+
+def pre_aggregation_pagination(df, groups, request):
+    '''Only aggregate the columns needed to perform sorting and pagination to filter out unneeded rows before the heavy lifting'''
+    if not request.sort_fields or request.sort_fields[0] == request.aggregation_group:
+        #only sort by group by column
+        ascending = True if not request.sort_fields else request.sort_ascending[0]
+        sorted_keys = df.index.drop_duplicates().sort_values(ascending=ascending)
+        
+    elif request.do_pivot:
+        pivot_sort_column = next(iter(request.sort_agg_params.keys()))
+        pre_pivot_column = pivot_sort_column.split('|')[-1]
+        sorted_keys = df.pivot_table(index=df.index, columns=request.pivot_columns, values=[pre_pivot_column], aggfunc=request.aggregation_params.get(pre_pivot_column))
+        sorted_keys.columns = ["|".join(str(f) for f in col[::-1]) for col in sorted_keys.columns] 
+        sorted_keys = sorted_keys.sort_values(by=request.sort_fields, ascending=request.sort_ascending).index
+        
+    else:    
+        agg_params = {**request.sort_agg_params, **{k:v for k,v in request.aggregation_params.items() if k in request.sort_fields}}
+        sorted_keys = (groups
+                       .agg(agg_params)
+                       .sort_values(by=list(request.sort_columns.keys()),
+                                    ascending=list(request.sort_columns.values()))
+                       ).index
+        
+    sorted_keys = sorted_keys[request.start_row:request.end_row]
+    df = df.loc[sorted_keys] #Filter out unneded groups before aggregating
+    return df, sorted_keys
+
+
+def sort_and_aggregate(df, request, response):
+    '''Sort, paginate and aggregate'''
     if request.aggregation_group:
         df = df.set_index(request.aggregation_group)
 
@@ -65,28 +91,7 @@ def sort_and_aggregate(df, request, response):
 
         #Do we need to paginate or sort on non aggregated field? If so, paginate before aggregating all columns. Huge speed up in some cases!!
         if pagination := request.end_row < last_row or request.start_row > 0 or request.sort_agg_params:
-
-            if not request.sort_fields or request.sort_fields[0] == request.aggregation_group:
-                #only sort by group by column
-                ascending = True if not request.sort_fields else request.sort_ascending[0]
-                sorted_keys = df.index.drop_duplicates().sort_values(ascending=ascending)
-            elif request.do_pivot:
-                pivot_sort_column = next(iter(request.sort_agg_params.keys()))
-                pre_pivot_column = pivot_sort_column.split('|')[-1]
-                sorted_keys = df.pivot_table(index=df.index, columns=request.pivot_columns, values=[pre_pivot_column], aggfunc=request.aggregation_params.get(pre_pivot_column))
-                sorted_keys.columns = ["|".join(str(f) for f in col[::-1]) for col in sorted_keys.columns] 
-                sorted_keys = sorted_keys.sort_values(by=request.sort_fields, ascending=request.sort_ascending).index
-
-            else:    
-                agg_params = {**request.sort_agg_params, **{k:v for k,v in request.aggregation_params.items() if k in request.sort_fields}}
-                sorted_keys = (groups
-                               .agg(agg_params)
-                               .sort_values(by=list(request.sort_columns.keys()),
-                                            ascending=list(request.sort_columns.values()))
-                               ).index
-                
-            sorted_keys = sorted_keys[request.start_row:request.end_row]
-            df = df.loc[sorted_keys] #Filter out unneded groups before aggregating
+            df, sorted_keys = pre_aggregation_pagination(df, groups, request)
 
         if request.do_pivot:
             df = pivot(df, request, response)
@@ -114,4 +119,3 @@ def process_filters(df, request):
     for field, filter_function in request.filters.items():
         df = df.loc[filter_function(df[field])]
     return df
-
